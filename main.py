d7b90e598962528acfc64ac7aeeb02477ba58bd7
Elisheva Aizen <154831898+Elisheva-Aizen@users.noreply.github.com>
2023-12-25 20:39:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ def main():
   
   ####Task_1####
   #get response for serial 1
-  response_1=rest_api_client.get_replay(1)
+  response_1=rest_api_client.get_data(1)
   print(response_1)
   #get response for serial 2
-  response_2=rest_api_client.get_replay(2)
+  response_2=rest_api_client.get_data(2)
   print(response_2)
   
   ####Task_2####
   #send posting request with the responses
-  response=rest_api_client.send_request(response_1,response_2)
+  response=rest_api_client.send_data(response_1,response_2)
   #check if posting is done successfully
   assert(response.ok)
   print ("correct")
