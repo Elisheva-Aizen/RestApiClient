5cfc77dd311a3e868a90c28b38ac4219cdf52d56
Elisheva Aizen <154831898+Elisheva-Aizen@users.noreply.github.com>
2023-12-26 11:44:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ def main():
   
   ####Task_1####
   #get response for serial 1
-  response_1=rest_api_client.get_data(1)
-  print(response_1)
-  #get response for serial 2
-  response_2=rest_api_client.get_data(2)
-  print(response_2)
-  
-  ####Task_2####
-  #send posting request with the responses
-  assert response_1 is not None and response_2 is not None,"response_1 or response_2 is None"
   try:
+    response_1=rest_api_client.get_data(1)
+    print(response_1)
+    #get response for serial 2
+    response_2=rest_api_client.get_data(2)
+    print(response_2)
+  
+    ####Task_2####
+    #send posting request with the responses
     response=rest_api_client.send_data(response_1,response_2)
     #check if posting is done successfully
     assert response.status_code=="200","Posting Request Failed"
