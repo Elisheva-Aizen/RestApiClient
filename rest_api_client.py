189ac5e073b4bbe02c2a93a192159a7c44d13f7c
Elisheva Aizen <154831898+Elisheva-Aizen@users.noreply.github.com>
2024-01-02 12:16:42 +0200

    Update rest_api_client.py

--- rest_api_client.py
+++ rest_api_client.py
@@ -19,11 +19,11 @@ class RestAPIClient:
             response.raise_for_status() #raise an http error
             return response.json()
         except requests.exceptions.HTTPError as errh:
-            raise Exception ("Get request http error:",str(errh))
+          print(f"Get request http error:",str(errh))
         except requests.exceptions.RequestException as errr:
-            raise Exception ("Error occured while processing get request:",str(errr))
+            print (f"Error occured while processing get request:",str(errr))
         except Exception as err:
-            raise Exception(str(err))
+            print(str(err))
         
     def send_data(self,response_1,response_2,serial,date,version): #api post request
         def get_json(): #get JSON data
@@ -51,15 +51,15 @@ class RestAPIClient:
                 }
                 return data
             except Exception as err:
-               raise Exception("Failed to get json data:",str(err))
+               print(f"Failed to get json data:",str(err))
         try:
             data=get_json()    
             response = requests.post(f"{self.base_url}/api/process", json=data,timeout=30)
             response.raise_for_status()
             return response
         except requests.exceptions.HTTPError as errh:
-            raise Exception ("Post request http error:",str(errh))
+            print(f"Post request http error:",str(errh))
         except requests.exceptions.RequestException as errr:
-            raise Exception ("Error occured while processing post request:" ,str(errr))
+            print(f"Error occured while processing post request:" ,str(errr))
         except Exception as err:
-               raise Exception(str(err))
+               print(str(err))
