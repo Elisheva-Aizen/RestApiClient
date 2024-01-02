0eb15ecb1c2e7ef44857ddd372298f6e68b7da90
Elisheva Aizen <154831898+Elisheva-Aizen@users.noreply.github.com>
2024-01-02 12:21:06 +0200

    Update rest_api_client.py

--- rest_api_client.py
+++ rest_api_client.py
@@ -18,11 +18,11 @@ class RestAPIClient:
             response = requests.get(f"{self.base_url}/api/responses?serial={serial}",timeout=30)
             response.raise_for_status() #raise an http error
             return response.json()
-        except requests.exceptions.HTTPError as errh:
+        except requests.exceptions.HTTPError as errh: #hadle an http error
           print(f"Get request http error:",str(errh))
-        except requests.exceptions.RequestException as errr:
+        except requests.exceptions.RequestException as errr: #handle any other request exception
             print (f"Error occured while processing get request:",str(errr))
-        except Exception as err:
+        except Exception as err: #hadle an exception on response.json() statment
             print(str(err))
         
     def send_data(self,response_1,response_2,serial,date,version): #api post request
